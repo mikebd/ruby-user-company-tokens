bc88ae433ad420fccae00258488ed954ff7cc9fc
Michael Ben-David <mikebd@users.noreply.github.com>
2023-11-12 17:25:24 -0500

    Basic ArgumentError for User and Company initialize

--- lib/model/company.rb
+++ lib/model/company.rb
@@ -4,7 +4,17 @@
 class Company
   attr_reader :id, :name, :top_up, :email_status
 
+  # rubocop:disable Layout/LineLength
   def initialize(id:, name:, top_up:, email_status:)
+    raise ArgumentError, 'Company id must be an Integer' unless id.is_a?(Integer)
+    raise ArgumentError, 'Company name must be a String' unless name.is_a?(String)
+    raise ArgumentError, 'Company top_up must be an Integer' unless top_up.is_a?(Integer)
+    raise ArgumentError, 'Company email_status must be a Boolean' unless email_status.is_a?(TrueClass) || email_status.is_a?(FalseClass)
+
+    # TODO: Additional validations to consider
+    # - non-empty strings
+    # - top_up >= 0
+
     @id = id
     @name = name
     @top_up = top_up
@@ -12,6 +22,7 @@ class Company
 
     @users = []
   end
+  # rubocop:enable Layout/LineLength
 
   def to_s
     "id: #{@id}, name: #{@name}, top_up: #{@top_up}, \
--- lib/model/user.rb
+++ lib/model/user.rb
@@ -5,9 +5,22 @@ class User
   attr_reader :id, :first_name, :last_name, :email, :company_id, :email_status,
               :active_status, :tokens
 
-  # rubocop:disable Metrics/ParameterLists
+  # rubocop:disable Layout/LineLength, Metrics/*
   def initialize(id:, first_name:, last_name:, email:, company_id:,
                  email_status:, active_status:, tokens:)
+    raise ArgumentError, 'User id must be an Integer' unless id.is_a?(Integer)
+    raise ArgumentError, 'User first_name must be a String' unless first_name.is_a?(String)
+    raise ArgumentError, 'User last_name must be a String' unless last_name.is_a?(String)
+    raise ArgumentError, 'User email must be a String' unless email.is_a?(String)
+    raise ArgumentError, 'User company_id must be an Integer' unless company_id.is_a?(Integer)
+    raise ArgumentError, 'User email_status must be a Boolean' unless email_status.is_a?(TrueClass) || email_status.is_a?(FalseClass)
+    raise ArgumentError, 'User active_status must be a Boolean' unless active_status.is_a?(TrueClass) || active_status.is_a?(FalseClass)
+    raise ArgumentError, 'User tokens must be an Integer' unless tokens.is_a?(Integer)
+
+    # TODO: Additional validations to consider
+    # - non-empty strings
+    # - email format
+
     @id = id
     @first_name = first_name
     @last_name = last_name
@@ -17,8 +30,7 @@ class User
     @active_status = active_status
     @tokens = tokens
   end
-
-  # rubocop:enable Metrics/ParameterLists
+  # rubocop:enable Layout/LineLength, Metrics/*
 
   def to_s
     "id: #{@id}, first_name: #{@first_name}, last_name: #{@last_name}, \
