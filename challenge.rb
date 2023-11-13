a94e369342e64e6e80e6f30cd9358d2ad42d3e48
Michael Ben-David <mikebd@users.noreply.github.com>
2023-11-13 04:11:02 -0500

    Skeletal CompaniesController and challenge.rb

---
+++ challenge.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative 'lib/controller/companies_controller'
+
+COMPANIES_JSON_FILE = 'companies.json'
+USERS_JSON_FILE = 'users.json'
+OUTPUT_FILE = 'output.txt'
+
+# load the companies from the json files
+companies = CompaniesController.load_json(
+  COMPANIES_JSON_FILE,
+  USERS_JSON_FILE
+)
+
+# output the companies to the output file
+# File.open(OUTPUT_FILE, 'w') do |file|
+#  companies.each do |company|
+#    file.puts company.to_s
+#  end
+# end
---
+++ lib/controller/companies_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# CompaniesController is a stateless class that manages a set of Company objects
+class CompaniesController
+  def self.load_json(companies_filename, users_filename)
+    companies = Companies.new
+
+    load_companies_json(companies, companies_filename)
+    load_users_json(companies, users_filename)
+
+    companies
+  end
+
+  private_class_method def self.load_companies_json(companies, filename)
+    file = File.read(filename)
+    JSON.parse(file)
+
+    # TODO: Add companies
+  end
+
+  private_class_method def self.load_users_json(companies, filename)
+    file = File.read(filename)
+    JSON.parse(file)
+
+    # TODO: Add users to companies
+  end
+
+end
